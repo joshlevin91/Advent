09d8a83310467e456ac3d08615a992e67f4bc912
Joshua Levin <joshualevin91@gmail.com>
2020-12-05 13:33:37 -0500

    2020 Day 5

---
+++ 2020/Day05.py
@@ -0,0 +1,33 @@
+import re
+
+def ID(row, col):
+	return 8*row + col
+
+def binop(lowest, highest, ops):
+	if len(ops) == 1:
+		return int(lowest) if ops[0] == 'L' else int(highest)
+	elif ops[0] == 'L':
+		return binop(lowest, lowest + (highest - lowest - 1)/2, ops[1:])
+	else:
+		return binop(lowest + (highest - lowest + 1)/2, highest, ops[1:])
+
+open_seat_IDs = []
+[open_seat_IDs.append(ID(row, col)) for row in range(128) for col in range(8)]
+
+highest_ID = 0
+taken_seat_IDs = []
+
+lines = open("Day05.txt", 'r').read().split('\n')
+for line in lines:
+	row = binop(0, 127, re.sub('B', 'U', re.sub('F', 'L', line[0:7])))
+	col = binop(0, 7, re.sub('R', 'U', line[7:10]))
+	highest_ID = max(highest_ID, ID(row, col))
+	open_seat_IDs.remove(ID(row,col))
+	taken_seat_IDs.append(ID(row,col))
+
+for ID in open_seat_IDs:
+	if ID-1 in taken_seat_IDs and ID+1 in taken_seat_IDs:
+		my_ID = ID
+		break
+
+print(highest_ID, my_ID)
